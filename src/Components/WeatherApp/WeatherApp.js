5d27ed088a47cd481c6bf7f6c96165210e97f028
koanvl <koanvl@ukr.net>
2022-07-19 10:25:22 +0200

    fixed prefix

--- src/Components/WeatherApp/WeatherApp.js
+++ src/Components/WeatherApp/WeatherApp.js
@@ -5,7 +5,7 @@ import Footer from "../Footer/Footer";
 export default function WeatherApp(){
   return (
   <div>
-    <div className="container">
+    <div className="container container">
       <Weather/>
     </div>
     <Footer/>
